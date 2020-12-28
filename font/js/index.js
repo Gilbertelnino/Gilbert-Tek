3e31416d1eedaf93548680818c1a1e2ec508b239
jeanfidele1 <musirikarejf@gmail.com>
2020-12-28 23:25:14 +0200

    change navigation

--- font/js/index.js
+++ font/js/index.js
@@ -1,12 +1,17 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+const navLink = document.querySelectorAll(".nav-link");
+const menu = document.querySelector("#menu");
+const ul = document.querySelector("ul");
+const exit = document.querySelector(".exit-btn");
+const openMenu = () => {
+  ul.classList.toggle("hide-mobile");
+};
+const closeMenu = () => {
+  ul.classList.add("hide-mobile");
+};
+menu.addEventListener("click", openMenu);
+exit.addEventListener("click", closeMenu);
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    ul.classList.add("hide-mobile");
+  });
 });
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
